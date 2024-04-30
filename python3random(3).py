bcc1fe92cf03820747c9ea62cc7117f4dc680cca
microsoft02 <105100643+microsoft02@users.noreply.github.com>
2024-04-30 22:02:34 +0530

    Update python3random(3).py

--- python3random(3).py
+++ python3random(3).py
@@ -51,3 +51,40 @@ print('balance after deposit:',user1.check_bal())
 
 user1.withdraw(500)
 print('balance after withdraw:',user1.check_bal())
+
+
+# 1) What is the difference between append() and extend() methods in Python lists?
+# -> 'append()' use to insert single value at a time at the end of list. you can only give one value or parameter in this method.
+temp_list = [1, 2, 3]
+temp_list.append(4)
+print(temp_list)  # Output: [1, 2, 3, 4]
+
+# 'extend()' use to insert multiple value or can be single but it need iterable object which is tuple or list or other. it att value at the end of list.
+temp_list = [1, 2, 3]
+temp_list.extend([4, 5])
+print(temp_list)  # Output: [1, 2, 3, 4, 5]
+
+
+# 2) Explain the purpose of __init__ method in Python classes.
+# -> __init__ use to initialize the instance of class object. it also called it constructor too. when new object created, python call the init function automatically
+# and get the name and age of that user.
+
+class user:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Creating an instance of the Person class
+person1 = user("Alice", 10)
+
+# Accessing attributes of the instance
+print(person1.name)  # Output: Alice
+print(person1.age)   # Output: 10
+
+
+# 3) How does Python's garbage collection mechanism work, and what are some ways to manage memory in Python programs?
+# -> for garbage collection it deallocate the memory that is not in use. mean if there is no stored value then memory address get free (maybe) 
+# Python uses a technique called reference counting combined with a cycle-detecting garbage collector to manage memory.
+
+# for manage memory we can use generator that generate value when they actually need and called.
+# and clost the files or folder completely that is not in use. so the extra memory will get free.
