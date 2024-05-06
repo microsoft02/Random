cb84107d4b1c89087a1a1027adb72881b21c2c4c
microsoft02 <105100643+microsoft02@users.noreply.github.com>
2024-05-06 20:48:09 +0530

    Update javascript3random(5).js

--- javascript3random(5).js
+++ javascript3random(5).js
@@ -45,3 +45,40 @@ function prime(n){
 
 num=parseInt(prompt('enter number'))
 console.log(prime(num))
+
+
+//1) Explain the difference between arrays and objects in JavaScript.
+// -> array is collection of similar of different type of datatype. the array can be homogenous or heterogenous. array is represent by [] square bracket.
+// eg.
+mylist=['string',10,true]
+// object is collection of key and value pairs. object represent by {} curly brackets.
+// eg.
+myobj={'name':'micro','age':10}
+
+
+//2)What is the purpose of the constructor method in javascript classes?
+// ->  the purpose of cnstructor to intialize the instance of class object when object created constructor take values of object into argument at the start.
+// eg. 
+class bank{
+    constructor(name){
+        this.name=name
+    }
+    info(){
+        return this.name
+    }
+}
+
+user1=new bank('micro')
+console.log(user1.info())
+
+
+// 3)How does javascript handle exceptions, and what are some common built-in exceptions?
+// -> in javascript exception handle using try catch throw block. try use for code that you want to make catch the error. 
+// when error occur catch block catch the error like logging, signup and give error message and throw use to throw custom error it doesn't matter where you use it.
+// eg.
+try{
+    // code
+}catch{
+    console.error('error occure');
+}throw{
+}
